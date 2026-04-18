63bbfb891b492ebf9d3e6bf51458255902d2fd4c
gabriel <gabriel.notari@icloud.com>
2026-04-18 18:10:59 -0300

    modificações na tela de cadastro

--- app/src/main/java/br/com/fiap/flore/screens/SignupScreen.kt
+++ app/src/main/java/br/com/fiap/flore/screens/SignupScreen.kt
@@ -4,15 +4,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.RemoveRedEye
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -79,5 +93,112 @@ fun UserImage(modifier: Modifier = Modifier) {
 private fun ProfileImagePreview() {
     FloreTheme() {
         UserImage()
+    }
+}
+
+@Composable
+fun SignupUserForm(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.digite_o_seu_nome))
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedTextColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary
+                ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = stringResource(R.string.person_icon),
+                    tint = MaterialTheme.colorScheme.tertiary
+
+                )
+            }
+        )
+        //Caixa de texto para email
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.digite_seu_e_mail))
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedTextColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary
+                ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Mail,
+                    contentDescription = stringResource(R.string.email_icon),
+                    tint = MaterialTheme.colorScheme.tertiary
+
+                )
+            }
+        )
+        //Caixa de texto para senha
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.digite_sua_senha))
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedTextColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary
+                ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = stringResource(R.string.lock_icon),
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.RemoveRedEye,
+                    contentDescription = stringResource(R.string.removeredeye),
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.crie_uma_nova_conta)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignupUserFormPreview() {
+    FloreTheme() {
+        SignupUserForm()
     }
 }
